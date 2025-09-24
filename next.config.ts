3a2de4551d00615cf9444994928d419469e650fe
platon.zaharov <Udav251119690>
2025-09-24 15:03:50 +0300

    fix github pages

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/platre-sinlge' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/platre-sinlge/' : '',
 };
 
 export default nextConfig;
