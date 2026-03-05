f387259459dec5cc8e27a7c4ccd84f655197243e
platon.zaharov <Udav251119690>
2026-03-05 12:01:08 +0300

    disable some rulles eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,15 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-img-element": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+  {
     ignores: [
       "node_modules/**",
       ".next/**",
